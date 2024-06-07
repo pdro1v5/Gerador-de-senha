7a0fdf68e583ee726e9e28f3fbaafa3638da83e8
pdro1v5 <137305299+pdro1v5@users.noreply.github.com>
2024-06-07 19:02:40 -0300

    Update script.js

--- script.js
+++ script.js
@@ -4,15 +4,19 @@ let password = document.querySelector("#password");
 let containerPassword = document.querySelector("#container-password");
 let charset = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ01234567891";
 let novaSenha = ";
-sizePassword.innerHTML = sliderElement.value;
-sizePassword.innerHTML = this.value;}
-slider.oninput = function()
-function generatePassword()
-let pass = ";
+ sizePassword.innerHTML = sliderElement.value;
+ slider.oninput = function(){
+ sizePassword()}
+function generatePassword(){
+let pass =";
 for(let i = 0, n = charset.length; i < sliderElement.value; ++i){
 pass += charset.charAt(Math.floor(Math.random() * n));
+}
 console.log(pass)
 containerPassword.classList.remove("hide"); password.innerHTML = pass;
 novaSenha = pass;
-function copyPassword(){alert("Senha copiada com sucesso!")
+}
+function copyPassword(){
+alert("Senha copiada com sucesso!")
 navigator.clipboard.writeText(novaSenha) 
+}
